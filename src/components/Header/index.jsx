7c4a24e3e746803c8600cbcb97022f8a9eb5f861
Seier <seier.bernal@gmail.com>
2025-02-18 21:04:26 -0600

    Se crea componente header con sección hero

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+function Header() {
+    return (
+            <section className="hero-section" id="inicio">
+                <p className="heromotivation">“Primero resuelve el problema, después escribe el código”. <br/>– John Johnson.</p>
+                <video autoPlay muted loop id="heroVideo">
+                    <source src="/assets/estrellas.mp4" type="video/mp4" />
+                    Tu navegador no soporta videos HTML5.
+                </video>
+                </section>
+         );
+        }
+
+export default Header;
