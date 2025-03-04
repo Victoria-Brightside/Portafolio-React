40b2a1cf36d5013c0c70112c360879942d9603e1
Seier <seier.bernal@gmail.com>
2025-03-03 18:34:54 -0600

    Se agregan estilos e información a componente about me

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -4,20 +4,19 @@ function Aboutme() {
     return (
         <>
             <section className="about_container">
-                <article className="info_about">
-                <h1> Acerca de mí </h1>
-                <p class="about_info"> Soy Victoria Bernal, entusiasta del aprendizaje, las artes marciales y la
-                    tecnología.<br />
-                    Mi primera formación fue como médico ortopedista, actualmente me encuentro estudiando la ingenierá en sistemas computacionales.<br />
-                    Actualmente estoy participando en el
-                    proyecto Oracle One en Alura LATAM y estudio la Ingeniería en Sistemas Computacionales en la
-                    Universidad
-                    Virtual del Estado de Guanajuato</p>
+                <h1>Acerca de mí</h1>
+                <div className="content">
+                    <article className="info_about">
+                        <p className="info">
+                            Soy Victoria Bernal, entusiasta del aprendizaje, las artes marciales y la tecnología.<br/><br/>
+                            Mi primera formación fue como médico ortopedista, actualmente me encuentro estudiando la ingeniería en sistemas computacionales.<br/><br/>
+                            Actualmente estoy participando en el proyecto Oracle One en Alura LATAM y estudio la Ingeniería en Sistemas Computacionales en la Universidad Virtual del Estado de Guanajuato.
+                        </p>
                     </article>
                     <article className="about_photo">
-
+                        <img src="/assets/developer.jpg" className="photo" />
                     </article>
-
+                </div>
             </section>
         </>
     )
