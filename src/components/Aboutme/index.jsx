849ee620256ee52fe5b8566858ade8be2c44f596
Seier <seier.bernal@gmail.com>
2025-04-07 18:23:23 -0600

    Se actualiza información de sección about me

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -8,16 +8,20 @@ function Aboutme() {
                 <div className="content">
                     <article className="info_about">
                         <p className="info">
-                            Soy Victoria Bernal, entusiasta del aprendizaje, las artes marciales y la tecnología.<br/><br/>
-                            Mi primera formación fue como médico ortopedista, actualmente me encuentro estudiando la ingeniería en sistemas computacionales.<br/><br/>
-                            Actualmente estoy participando en el proyecto Oracle One en Alura LATAM y estudio la Ingeniería en Sistemas Computacionales en la Universidad Virtual del Estado de Guanajuato.
+                            ¡Hola! Soy <strong>Victoria Bernal</strong>, una apasionada del aprendizaje, la tecnología y las artes marciales.<br /><br />
+                            Mi trayectoria profesional comenzó en el campo de la <strong>medicina</strong>, donde me especialicé como <strong>médico ortopedista</strong>.
+                            Sin embargo, mi interés y pasión por la <strong>tecnología</strong> me llevaron a dar un giro en mi carrera, y actualmente estudio <strong>Ingeniería en Sistemas Computacionales</strong> en la Universidad Virtual del Estado de Guanajuato.<br /><br />
+                            Tuve la oportunidad de participar en el <strong>proyecto Oracle One en Alura LATAM (2024)</strong>, donde fortalecí mis habilidades en <strong>programación y desarrollo web</strong>.<br /><br />
                         </p>
                     </article>
                     <article className="about_photo">
                         <img src="/assets/developer.jpg" className="photo" />
+                        <p className="info">
+                            Si te interesa conocer más sobre mi trabajo o colaborar en algún proyecto, ¡estaré encantada de conectar contigo!<br /><br />
+                        </p>
                     </article>
                 </div>
-            </section>
+            </section >
         </>
     )
 }
