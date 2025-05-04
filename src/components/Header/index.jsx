af4093e190a9b25608e68d8399d9b07aff83b19f
Seier <seier.bernal@gmail.com>
2025-05-04 16:16:12 -0600

    Se modifica ruta de vide hero

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ function Header() {
             <section className="hero-section" id="inicio">
                 <p className="heromotivation">“Primero resuelve el problema, después escribe el código”. <br/>– John Johnson.</p>
                 <video autoPlay muted loop id="heroVideo">
-                    <source src="/assets/estrellas.mp4" type="video/mp4" />
+                    <source src="/Portafolio-React/assets/estrellas.mp4" type="video/mp4" />
                     Tu navegador no soporta videos HTML5.
                 </video>
                 <div className="rol">
